f71ec20e7acb3a2e778333182ab297c3783c24e3
anddp12 <anddp12@gmail.com>
2022-12-23 22:39:19 +0200

    + @property

--- main.py
+++ main.py
@@ -7,17 +7,25 @@ class Person:
     def __init__(self, wallet = '') -> None:
         self.__wallet = wallet
 
-    def setwallet(self, value):
-        print('setwallet')
-        self.__wallet = value
+    # def setwallet(self, value):
+    #     print('setwallet')
+    #     self.__wallet = value
 
-    def getwallet(self):
-        print('setwallet')
+    # def getwallet(self):
+    #     print('setwallet')
+    #     return self.__wallet
+    # wallet = property(getwallet, setwallet)
+
+    @property
+    def wallet(self):
         return self.__wallet
-    wallet = property(getwallet, setwallet)
+    
+    @wallet.setter
+    def wallet(self, value):
+        self.__wallet = value
 
 p1 = Person() 
 # p1.setwallet(10)
 # p1.getwallet()
-p1.wallet = 10
+p1.wallet = 100
 print(p1.wallet)
